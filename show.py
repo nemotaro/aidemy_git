945c6faf3fe4dfc4c2a2064089e7b88aaf479b86
nemotaro <w.4.univ@gmail.com>
2023-05-01 22:34:13 +0900

    change print

--- show.py
+++ show.py
@@ -7,7 +7,7 @@ def main():
 
     with open("./hyakunin.txt", encoding="utf-8") as f:
         wakas = [s.strip() for s in f.readlines()]
-        print("今日の一句" + wakas[random.randrange(len(wakas))])
+        print("今日の一句なり" + wakas[random.randrange(len(wakas))])
 
 if __name__ == '__main__':
     main()
